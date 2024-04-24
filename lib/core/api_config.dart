2b0530854dba8205c8f95cfd92cfc1e461686178
abdo1234510 <92454005+Abdelrahman-Wael-1029@users.noreply.github.com>
2024-04-24 13:17:14 +0200

    add models

---
+++ lib/core/api_config.dart
@@ -0,0 +1,12 @@
+
+String baseUrl = "https://www.themealdb.com/api/json/v1/1/";
+
+String searchMeal = "${baseUrl}search.php?"; // search.php?s=Arrabiata  || s="" for all meals
+String detailsMeal = "${baseUrl}lookup.php?"; // meal.php?i=52772
+String listCategories = "${baseUrl}categories.php"; 
+String filterMealsByCategory = "${baseUrl}filter.php?"; // filter.php?c=Seafood
+String randomMeal = "${baseUrl}random.php"; // random.php
+
+
+
+
---
+++ lib/data/model/meal.dart
@@ -0,0 +1,44 @@
+class Meal {
+  String id;
+  String name;
+  String category;
+  double price;
+  String imageUrl;
+  List<String> tags;
+
+  Meal({
+    required this.id,
+    required this.name,
+    required this.price,
+    required this.category,
+    required this.imageUrl,
+    required this.tags,
+  });
+
+  factory Meal.fromJson(Map<String, dynamic> json) {
+    return Meal(
+      id: json['idMeal'],
+      name: json['strMeal'],
+      price: 0.0,
+      category: json['strCategory'],
+      imageUrl: json['strMealThumb'],
+      tags: List<String>.from(json['strTags'].split(', ')),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'idMeal': id,
+      'strMeal': name,
+      'strCategory': category,
+      'price': 0.0,
+      'strMealThumb': imageUrl,
+      'strTags': tags.join(', '),
+    };
+  }
+
+  @override
+  String toString() {
+    return 'Meal{id: $id, name: $name, price: $price, category: $category, imageUrl: $imageUrl, tags: $tags}';
+  }
+}
