0ec5abc6cdb622c25a8b3cb72578a9ec06bf51f8
abdo1234510 <92454005+Abdelrahman-Wael-1029@users.noreply.github.com>
2024-04-28 08:43:58 +0300

    category details (add info category)

--- lib/view/category_details/screen/category_details.dart
+++ lib/view/category_details/screen/category_details.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:meal_app/common/widget/image.dart';
+import 'package:meal_app/core/color_manager.dart';
+import 'package:meal_app/core/value_manager.dart';
 import 'package:meal_app/data/model/category.dart';
 import 'package:meal_app/data/model/meal_category.dart';
 import 'package:meal_app/view/meal_details/screens/meal_details.dart';
@@ -26,31 +28,61 @@ class CategoryDetails extends StatelessWidget {
         surfaceTintColor: Colors.transparent,
         backgroundColor: Colors.transparent,
       ),
-      body: ListView.separated(
-        separatorBuilder: (context, index) => const Divider(),
-        itemCount: meals.length,
-        itemBuilder: (context, index) {
-          final meal = meals[index];
-          return ListTile(
-            leading: CircleAvatar(
-              radius: 30,
-              child: getImage(
-                url: meal.imageUrl,                
+      body: Padding(
+        padding: EdgeInsets.all(PaddingManager.p8),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              getImage(
+                url: category.imageUrl,
+                fit: BoxFit.contain,
+                width: double.infinity,
+                height: 200,
               ),
-            ),
-            titleTextStyle: Theme.of(context).textTheme.headlineMedium,
-            title: Text(meal.name),
-            onTap: () async {
-              cubit.getMealById(meal.id).then((value) {
-                Navigator.pushNamed(
-                  context,
-                  MealDetails.routeName,
-                  arguments: value,
-                );
-              });
-            },
-          );
-        },
+              SizedBox(
+                height: SizeManager.s8,
+              ),
+              Text(
+                category.description,
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color: ColorManager.grey,
+                    ),
+              ),
+              SizedBox(
+                height: SizeManager.s8,
+              ),
+              ListView.separated(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                separatorBuilder: (context, index) => const Divider(),
+                itemCount: meals.length,
+                itemBuilder: (context, index) {
+                  final meal = meals[index];
+                  return ListTile(
+                    contentPadding: EdgeInsets.zero,
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: getImage(
+                        url: meal.imageUrl,
+                      ),
+                    ),
+                    titleTextStyle: Theme.of(context).textTheme.headlineMedium,
+                    title: Text(meal.name),
+                    onTap: () async {
+                      cubit.getMealById(meal.id).then((value) {
+                        Navigator.pushNamed(
+                          context,
+                          MealDetails.routeName,
+                          arguments: value,
+                        );
+                      });
+                    },
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
